b8b7eb9ebd7005572b16fa60714bb1ca4e190ebe
Isabel Hirama <isabel.hirama@gmail.com>
2018-07-05 12:26:27 -0400

    stuck on adding a dispatch

--- game/index.js
+++ game/index.js
@@ -2,15 +2,16 @@ import {Map} from 'immutable'
 
 const MOVE = 'MOVE'
 
-const move = (position, player) => {
+const move = (player, position) => {
   return {type: MOVE, position: position, player: player}
 }
+
 
 const initialState = {board: Map()}
 
 export default function reducer(state = initialState, action) {
   if (action.type === 'MOVE') {
-    return {board: state.board.setIn(action.position, action.player)}
+    return {board: state.board.setIn(action.player, action.position)}
   }
   return state
 }
